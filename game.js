b1484c4e0c2ec43fe5c3b0593001bd0e97554284
NatiNegussie <136466532+NatiNegussie@users.noreply.github.com>
2023-08-12 15:12:27 +0300

    Add files via upload

--- game.js
+++ game.js
@@ -5,9 +5,144 @@ function mybtn() {
         document.getElementById("demo").innerHTML = "ሰማያዊ ፊኛ አሸንፈዋል";
         document.getElementById("demo").style.color = "green";
  document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+ document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('car').style.backgroundColor = "white";
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
     }
-    else {
-        alert("badlucj")
+    if(document.getElementById("num").value == 1)
+       {
+        document.getElementById("bear").style.backgroundColor = "lightgreen";
+        document.getElementById("demo").innerHTML = " ድብ አሸንፈዋል";
+        document.getElementById("demo").style.color = "green";
+ document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+ document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('car').style.backgroundColor = "white";
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('blue').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
     }
+    if(document.getElementById("num").value == 2)
+    {
+     document.getElementById("bicycle").style.backgroundColor = "lightgreen";
+     document.getElementById("demo").innerHTML = " ሳይክል አሸንፈዋል";
+  document.getElementById("demo").style.color = "green";
+document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('car').style.backgroundColor = "white";
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('blue').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
+ }
+ if(document.getElementById("num").value == 3)
+    {
+     document.getElementById("dog").style.backgroundColor = "lightgreen";
+     document.getElementById("demo").innerHTML = " ዉሻ አሸንፈዋል";
+   document.getElementById('blue').style.backgroundColor = 'white'; 
+    document.getElementById("demo").style.color = "green";
+document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('car').style.backgroundColor = "white";
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('blue').style.backgroundColor = 'white';
+
+ }
+ if(document.getElementById("num").value == 4)
+    {
+    document.getElementById('blue').style.backgroundColor = 'white';
+     document.getElementById("green").style.backgroundColor = "lightgreen";
+     document.getElementById("demo").innerHTML = " አረንጓዴ ፊኛ አሸንፈዋል";
+     document.getElementById("demo").style.color = "green";
+document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('car').style.backgroundColor = "white";
+
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
+document.getElementById('blue').style.backgroundColor = 'white';
+ }
+ if(document.getElementById("num").value == 6)
+    {
+     document.getElementById("ballons").style.backgroundColor = "lightgreen";
+     document.getElementById("demo").innerHTML = "  ፊኛዎችን አሸንፈዋል";
+     document.getElementById("demo").style.color = "green";
+document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('car').style.backgroundColor = "white";
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('blue').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
+ }if(document.getElementById("num").value == 7)
+ {
+  document.getElementById("car").style.backgroundColor = "lightgreen";
+  document.getElementById("demo").innerHTML = "መኪና አሸንፈዋል";
+  document.getElementById("demo").style.color = "green";
+document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('blue').style.backgroundColor = 'white';
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
+}
+if(document.getElementById("num").value == 8)
+    {
+     document.getElementById("helicopter").style.backgroundColor = "lightgreen";
+     document.getElementById("demo").innerHTML = " ሄሊኮፕተር አሸንፈዋል";
+     document.getElementById("demo").style.color = "green";
+document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+document.getElementById('blue').style.backgroundColor = 'white';
+document.getElementById('car').style.backgroundColor = "white";
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
+ }
+ if(document.getElementById("num").value == 9)
+    {
+     document.getElementById("badlack").style.backgroundColor = "lightgreen";
+     document.getElementById("demo").innerHTML = " ምንም አላሸነፉም";
+    document.getElementById('blue').style.backgroundColor = 'white'; document.getElementById("demo").style.color = "green";
+document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('car').style.backgroundColor = "white";
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
+ }
+ if(document.getElementById("num").value == 10)
+ {
+  document.getElementById("badlack").style.backgroundColor = "lightgreen";
+  document.getElementById("demo").innerHTML = "ምንም አላሸነፉም";
+  document.getElementById("demo").style.color = "green";
+document.getElementById("btn").innerHTML = "ሌላ ይሞክሩ";
+const divs = [document.getElementById('helicopter'), document.getElementById('lasttoy'), document.getElementById('green'), document.getElementById('baloons'),document.getElementById('bicycle'), document.getElementById('bear')];
+document.getElementById('helicopter').style.backgroundColor = "white";
+document.getElementById('car').style.backgroundColor = "white";
+document.getElementById('green').style.backgroundColor = "white";
+document.getElementById('bicycle').style.backgroundColor = 'white';
+document.getElementById('ballons').style.backgroundColor = 'white';
+document.getElementById('bear').style.backgroundColor = 'white';
+document.getElementById('dog').style.backgroundColor = 'white';
+document.getElementById('blue').style.backgroundColor = 'white';
+ }
 }
 
--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function myfunc() {
-    document.getElementById("btn").style.animationName += "nmy";
+    document.getElementById("btn").style.animationName == "nmy";
 }
+document.getElementById('tech').addEventListener('click', function(){
+    alert('not made yet');
+});
